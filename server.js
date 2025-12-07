92779c525d87cc924365682707e20f51abdf8526
xayotxurshitsatvaldiyev-cmd <xayotxurshitsatvaldiyev@gmail.com>
2025-12-07 22:55:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public')); // frontend fayllari shu papkada bo'ladi
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // username va xabar kelganda barchaga tarqatish
     socket.on('chat message', (data) => {
-        io.emit('chat message', data); // data = { username, message }
+        io.emit('chat message', data);
     });
 
     socket.on('disconnect', () => {
@@ -21,7 +20,9 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+// Render tomonidan berilgan portni ishlatish
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+
